d671b613abc2fbf261a7fc3537876e834de666f7
Abdulmajeed-Fahad <majeed142425@gmail.com>
2026-07-18 20:27:56 +0300

    First CPP program

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello Majeed!";
+    return 0;
+}
---
+++ newfile.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+string M ;
+cin>>M;
+cout<<"where are you now ?";
+
+
+}
